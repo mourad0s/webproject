af95dfaa3866b0f054afcabaa67d6de0a6c2b3ff
mourad0s <mourad.zerrouki@etu.univ-amu.fr>
2025-09-04 15:01:09 +0200

    feat: dynamic page titles and active link highlighting

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():  # put application's code here
-    return render_template('index.html')
+    return render_template('index.html', active_page="home", title='Accueil')
 
 @app.route('/a-propos')
 def about():
-    return render_template('a-propos.html')
+    return render_template('a-propos.html', active_page="about", title='A propos')
 
 if __name__ == '__main__':
     app.run(debug=True)
